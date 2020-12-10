85785264e26f9e3d12fe59db443c78002bedfd40
Bojan Vrhovnik <bojan@vrhovnik.net>
2020-12-10 17:42:59 +0100

    getting back data from Dynamics from account and contacts

--- src/DynamicsDemo/DynamicsDemo.Cli/Program.cs
+++ src/DynamicsDemo/DynamicsDemo.Cli/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.PowerPlatform.Cds.Client;
+﻿using System;
+using Microsoft.PowerPlatform.Cds.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using Spectre.Console;
 
 namespace DynamicsDemo.Cli
@@ -8,12 +11,45 @@ namespace DynamicsDemo.Cli
         static void Main(string[] args)
         {
             HorizontalRule("Getting data from CDS");
-            //string connString = "AuthType=Office365;Username=admin@M365B537047.onmicrosoft.com;Password=7n8R2zNpZV;Url=https://m365b537047.crm.dynamics.com/";
-            var connString = "AuthType=OAuth;Username=admin@M365B537047.onmicrosoft.com;Password=7n8R2zNpZV;Url=https://m365b537047.crm.dynamics.com;AppId=e8461075-fab8-43f0-9c13-a4c60c97bdd5;RedirectUri=http://localhost;ClientId=e8461075-fab8-43f0-9c13-a4c60c97bdd5;ClientSecret=t~4Jcn4cpL.X0L700BTpwa~6hdcRJ.tk6f;LoginPrompt=Auto";
+            var connString = Environment.GetEnvironmentVariable("CDSConnString");
 
             var svc = new CdsServiceClient(connString);
             var myCdsUserId = svc.GetMyCdsUserId();
             AnsiConsole.WriteLine("My user ID is " + myCdsUserId);
+
+            HorizontalRule("Getting back account with primary contacts");
+
+            var qe = new QueryExpression {EntityName = "account", ColumnSet = new ColumnSet()};
+            qe.ColumnSet.Columns.Add("name");
+
+            qe.LinkEntities.Add(new LinkEntity("account", "contact", "primarycontactid", "contactid",
+                JoinOperator.Inner));
+            qe.LinkEntities[0].Columns.AddColumns("firstname", "lastname");
+            qe.LinkEntities[0].EntityAlias = "primarycontact";
+
+            var ec = svc.RetrieveMultiple(qe);
+
+            AnsiConsole.WriteLine($"Retrieved {ec.Entities.Count} entities");
+
+            var projectRepresentation = new Table()
+                .Border(TableBorder.Square)
+                .BorderColor(Color.Red)
+                .AddColumn(new TableColumn("Name"))
+                .AddColumn(new TableColumn("First Name"))
+                .AddColumn(new TableColumn("Last Name"));
+
+            foreach (var act in ec.Entities)
+            {
+                var accountName = act["name"]?.ToString();
+                var firstName = act.GetAttributeValue<AliasedValue>("primarycontact.firstname")?.Value?.ToString();
+                var lastName = act.GetAttributeValue<AliasedValue>("primarycontact.lastname")?.Value?.ToString();
+
+                projectRepresentation.AddRow(accountName ?? "", firstName ?? "", lastName ?? "");
+            }
+
+            AnsiConsole.Render(projectRepresentation);
+
+            Console.WriteLine("Retrieved {0} entities", ec.Entities.Count);
         }
 
         private static void HorizontalRule(string title)
